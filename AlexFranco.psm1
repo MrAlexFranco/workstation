b7c966fc1ee7dc924d22e063b20130b0d5549719
Alex Franco <alex@franco.dev>
2021-08-19 09:51:29 -0500

    updated get-telnet

--- AlexFranco.psm1
+++ AlexFranco.psm1
@@ -203,32 +203,38 @@ function Get-MACVendor {
 
 Function Get-Telnet {
     Param (
-        [Parameter(ValueFromPipeline = $true)]
-        [String[]]$Commands = @("username", "password", "disable clipaging", "sh config"),
-        [string]$RemoteHost = "HostnameOrIPAddress",
+        [Parameter(Mandatory, ValueFromPipeline = $true)]
+        [String[]]$Commands,
+
+        [Parameter(Mandatory)]
+        [string]$RemoteHost,
+
         [string]$Port = "23",
         [int]$WaitTime = 1000,
-        [string]$OutputPath = "\\server\share\switchbackup.txt"
+        [string]$OutputPath
     )
-    #Attach to the remote device, setup streaming requirements
+
+    # Attach to the remote device, setup streaming requirements
     $Socket = New-Object System.Net.Sockets.TcpClient($RemoteHost, $Port)
+
     If ($Socket) {
         $Stream = $Socket.GetStream()
         $Writer = New-Object System.IO.StreamWriter($Stream)
         $Buffer = New-Object System.Byte[] 1024 
         $Encoding = New-Object System.Text.AsciiEncoding
 
-        #Now start issuing the commands
+        # Now start issuing the commands
         ForEach ($Command in $Commands) {
             $Writer.WriteLine($Command) 
             $Writer.Flush()
             Start-Sleep -Milliseconds $WaitTime
         }
-        #All commands issued, but since the last command is usually going to be
-        #the longest let's wait a little longer for it to finish
+
+        # All commands issued, but since the last command is usually going to be
+        # the longest let's wait a little longer for it to finish
         Start-Sleep -Milliseconds ($WaitTime * 4)
         $Result = ""
-        #Save all the results
+        # Save all the results
         While ($Stream.DataAvailable) {
             $Read = $Stream.Read($Buffer, 0, 1024) 
             $Result += ($Encoding.GetString($Buffer, 0, $Read))
@@ -236,6 +242,10 @@ Function Get-Telnet {
     } Else {
         $Result = "Unable to connect to host: $($RemoteHost):$Port"
     }
-    #Done, now save the results to a file
-    $Result | Out-File $OutputPath
+    
+    if ($OutputPath) {
+        $Result | Out-File $OutputPath
+    }
+
+    return $Result
 }
