801906f93ce8248476abbe5a607fd58385474172
Alex Franco <MrAlexFranco@users.noreply.github.com>
2024-04-23 11:16:50 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -3,30 +3,30 @@ Clear-Host
 # Region Custom Prompt
 oh-my-posh init pwsh --config $env:USERPROFILE\.material.json | Invoke-Expression
 
-# Remove curl alias so it won't interfere with installed curl
+# Remove curl alias so it won"t interfere with installed curl
 Remove-Item -Path Alias:\curl -Force -ErrorAction SilentlyContinue
 
 # Set-PSReadLineOptions
 $PSreadLineOption = @{
     HistoryNoDuplicates = $true
-    PredictionSource    = 'History'
-    PredictionViewStyle = 'ListView'
+    PredictionSource    = "History"
+    PredictionViewStyle = "ListView"
 }
 
 Set-PSReadLineOption @PSReadLineOption
 
 # Set Vars
-New-Item -Path Env:\ -Name 'CODE' -Value "$env:USERPROFILE\OneDrive - Franco.dev\Code" -Force | Out-Null
+New-Item -Path Env:\ -Name "CODE" -Value "$env:USERPROFILE\OneDrive - Franco.dev\Code" -Force | Out-Null
 Set-Alias -Name ~ -Value $env:USERPROFILE
 
-if (Get-Secret -Name 'OpenAIKey') { $env:OpenAIKey = Get-Secret -Name 'OpenAIKey' -AsPlainText }
+if (Get-Secret -Name "OpenAIKey") { $env:OpenAIKey = Get-Secret -Name "OpenAIKey" -AsPlainText }
 
 Set-Location $env:CODE
 
 # $PSDefaultParameterValues
-$PSDefaultParameterValues.Add('Format-Table:AutoSize', $true)
+$PSDefaultParameterValues.Add("Format-Table:AutoSize", $true)
 
-if ($env:USERDNSDOMAIN -eq 'WECI.NET') {
-    $PSDefaultParameterValues.Add('Invoke-Command:Credential', (Get-Secret -Name 'afranco-admin(PSCredential)'))
-    $PSDefaultParameterValues.Add('Enter-PSSession:Credential', (Get-Secret -Name 'afranco-admin(PSCredential)'))    
+if ($env:USERDNSDOMAIN -eq "WECI.NET") {
+    $PSDefaultParameterValues.Add("Invoke-Command:Credential", (Get-Secret -Name "afranco-admin"))
+    $PSDefaultParameterValues.Add("Enter-PSSession:Credential", (Get-Secret -Name "afranco-admin"))
 }
