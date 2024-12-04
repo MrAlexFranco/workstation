dc413092d93fbf19eb1148a5ba7c512b0f0088d4
Alex Franco <MrAlexFranco@users.noreply.github.com>
2024-12-04 10:04:27 -0600

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,10 +1,12 @@
 Clear-Host
 
-# Region Custom Prompt
-oh-my-posh init pwsh --config $env:USERPROFILE\.material.json | Invoke-Expression
+# $PSDefaultParameterValues
+$PSDefaultParameterValues.Add("Format-Table:AutoSize", $true)
 
-# Remove curl alias so it won"t interfere with installed curl
-Remove-Item -Path Alias:\curl -Force -ErrorAction SilentlyContinue
+if ($env:USERDNSDOMAIN -eq "WECI.NET") {
+    $PSDefaultParameterValues.Add("Invoke-Command:Credential", (Get-Secret -Name "afranco-admin"))
+    $PSDefaultParameterValues.Add("Enter-PSSession:Credential", (Get-Secret -Name "afranco-admin"))
+}
 
 # Set-PSReadLineOptions
 $PSreadLineOption = @{
@@ -19,14 +21,9 @@ Set-PSReadLineOption @PSReadLineOption
 New-Item -Path Env:\ -Name "CODE" -Value "$env:USERPROFILE\OneDrive - Franco.dev\Code" -Force | Out-Null
 Set-Alias -Name ~ -Value $env:USERPROFILE
 
-if (Get-Secret -Name "OpenAIKey") { $env:OpenAIKey = Get-Secret -Name "OpenAIKey" -AsPlainText }
+# Region Custom Prompt
+if (Test-Path -Path "$env:USERPROFILE\.material.json") {
+    oh-my-posh init pwsh --config "$env:USERPROFILE\.material.json" | Invoke-Expression
+}
 
 Set-Location $env:CODE
-
-# $PSDefaultParameterValues
-$PSDefaultParameterValues.Add("Format-Table:AutoSize", $true)
-
-if ($env:USERDNSDOMAIN -eq "WECI.NET") {
-    $PSDefaultParameterValues.Add("Invoke-Command:Credential", (Get-Secret -Name "afranco-admin"))
-    $PSDefaultParameterValues.Add("Enter-PSSession:Credential", (Get-Secret -Name "afranco-admin"))
-}
