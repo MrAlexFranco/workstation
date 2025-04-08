b5d84fdf3ae8c68b0a59cd7a37a2185c3df52b9f
Alex Franco <MrAlexFranco@users.noreply.github.com>
2025-04-08 09:43:37 -0500

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,29 +1,22 @@
-Clear-Host
-
 # $PSDefaultParameterValues
 $PSDefaultParameterValues.Add("Format-Table:AutoSize", $true)
+$PSDefaultParameterValues.Add("Export-Excel:AutoSize", $true)
+$PSDefaultParameterValues.Add("Export-Excel:FreezeTopRow", $true)
+$PSDefaultParameterValues.Add("Export-Excel:BoldTopRow", $true)
+$PSDefaultParameterValues.Add("Export-Excel:TableStyle", "None")
 
 if ($env:USERDNSDOMAIN -eq "WECI.NET") {
-    $PSDefaultParameterValues.Add("Invoke-Command:Credential", (Get-Secret -Name "afranco-admin"))
-    $PSDefaultParameterValues.Add("Enter-PSSession:Credential", (Get-Secret -Name "afranco-admin"))
+    $Credential = Get-Secret -Name "afranco-admin"
+    $PSDefaultParameterValues.Add("Invoke-Command:Credential", $Credential)
+    $PSDefaultParameterValues.Add("Enter-PSSession:Credential", $Credential)
 }
 
 # Set-PSReadLineOptions
-$PSreadLineOption = @{
-    HistoryNoDuplicates = $true
-    PredictionSource    = "History"
-    PredictionViewStyle = "ListView"
-}
-
-Set-PSReadLineOption @PSReadLineOption
+Set-PSReadLineOption -HistoryNoDuplicates $true -PredictionSource "History" -PredictionViewStyle "ListView"
 
 # Set Vars
-New-Item -Path Env:\ -Name "CODE" -Value "$env:USERPROFILE\OneDrive - Franco.dev\Code" -Force | Out-Null
-Set-Alias -Name ~ -Value $env:USERPROFILE
+New-Item -Path "Env:\" -Name "CODE" -Value "$env:USERPROFILE\OneDrive - Franco.dev\Code" -Force | Out-Null
+Set-Location -Path $env:CODE
 
 # Region Custom Prompt
-if (Test-Path -Path "$env:USERPROFILE\.material.json") {
-    oh-my-posh init pwsh --config "$env:USERPROFILE\.material.json" | Invoke-Expression
-}
-
-Set-Location $env:CODE
+oh-my-posh init pwsh | Invoke-Expression
