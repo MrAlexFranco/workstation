aa7e38164b16f30003e8e80d3efdc3008ee7c5b6
Alex Franco <MrAlexFranco@users.noreply.github.com>
2026-04-10 14:17:01 -0500

    Display exported functions in PowerShell profile
    Add a reminder for exported functions from AlexFranco module.

--- profile.ps1
+++ profile.ps1
@@ -11,5 +11,9 @@ Set-PSReadLineOption -HistoryNoDuplicates:$true -PredictionSource "History" -Pre
 New-Item -Path "Env:\" -Name "CODE" -Value "$HOME\Code" -Force | Out-Null
 Set-Location -Path $env:CODE
 
-# Prompt
-oh-my-posh init pwsh --config "$env:USERPROFILE\material.omp.json" | Invoke-Expression
+# Remind myself what functions I've already written
+$ExportedFunctions = Get-Module -Name "AlexFranco" -ListAvailable | Select-Object -ExpandProperty "ExportedFunctions" | Select-Object -ExpandProperty "Keys"
+Write-Host "Exported functions from AlexFranco module:" -ForegroundColor Cyan
+for ($n = 0; $n -lt $ExportedFunctions.Count; $n += 5) {
+    Write-Host " $($ExportedFunctions[$n..($n + 4)] -join ", ")" -ForegroundColor Cyan
+}
